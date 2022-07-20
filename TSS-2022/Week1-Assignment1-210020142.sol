ec5cb33af725deef22df9be703f3179a1d7501c9
Swadine <96530421+Swadine@users.noreply.github.com>
2022-07-20 14:46:03 +0530

    Create Week1-Assignment1-210020142.sol

---
+++ TSS-2022/Week1-Assignment1-210020142.sol
@@ -0,0 +1,18 @@
+```solidity
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.8;
+
+contract Bank {
+    uint256 TotalBalance;
+    mapping(address => uint256) Accounts;
+
+    function ShowTotalBalance() public view returns(uint256) {
+        return TotalBalance;
+    }
+
+    function ShowBalance(address UserAddress) public view returns(uint256) {
+        return Accounts[UserAddress];
+    }
+}
+```
